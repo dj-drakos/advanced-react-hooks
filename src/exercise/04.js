9e788920bec98b47114e6877d8d1afd872019b5b
DJ Drakos <dianajodrakos@gmail.com>
2022-12-22 15:39:34 -0800

    complete 04

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,12 +1,8 @@
-// useLayoutEffect: auto-scrolling textarea
-// http://localhost:3000/isolated/exercise/04.js
-
 import * as React from 'react'
 
 function MessagesDisplay({messages}) {
   const containerRef = React.useRef()
-  // 🐨 replace useEffect with useLayoutEffect
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     containerRef.current.scrollTop = containerRef.current.scrollHeight
   })
 
@@ -22,18 +18,14 @@ function MessagesDisplay({messages}) {
   )
 }
 
-// this is to simulate major computation/big rendering tree/etc.
 function sleep(time = 0) {
   const wakeUpTime = Date.now() + time
   while (Date.now() < wakeUpTime) {}
 }
 
 function SlooooowSibling() {
-  // try this with useLayoutEffect as well to see
-  // how it impacts interactivity of the page before updates.
   React.useEffect(() => {
-    // increase this number to see a more stark difference
-    sleep(300)
+    sleep(800)
   })
   return null
 }
